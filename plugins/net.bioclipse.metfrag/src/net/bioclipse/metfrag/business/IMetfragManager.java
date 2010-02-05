17a214975b636a369deed03ebecdae2ec718e994
Steffen Neumann <sneumann@ipb-halle.de>
2010-02-05 14:54:29 +0100

    now with adapter between ISpectrum, IMolecule and MetFrag

--- plugins/net.bioclipse.metfrag/src/net/bioclipse/metfrag/business/IMetfragManager.java
+++ plugins/net.bioclipse.metfrag/src/net/bioclipse/metfrag/business/IMetfragManager.java
@@ -12,11 +12,11 @@ package net.bioclipse.metfrag.business;
 
 import java.util.List;
 
-import org.eclipse.core.runtime.IProgressMonitor;
-
 import net.bioclipse.core.PublishedClass;
 import net.bioclipse.core.PublishedMethod;
 import net.bioclipse.core.business.BioclipseException;
+import net.bioclipse.core.domain.IMolecule;
+import net.bioclipse.core.domain.ISpectrum;
 import net.bioclipse.jobs.IReturner;
 import net.bioclipse.managers.business.IBioclipseManager;
 
@@ -24,7 +24,25 @@ import net.bioclipse.managers.business.IBioclipseManager;
     value="The MetFrag manager is the interface to the in-silico Fragmentation tool."
 )
 public interface IMetfragManager extends IBioclipseManager {
-	@PublishedMethod(params = "", 
-	                 methodSummary = "Runs a pre-canned query against KEGG :-)")
-	public List<Double> calculateMetFragScore() throws BioclipseException;
+
+	@PublishedMethod(params = "Double mzabs, Double mzppm", 
+            methodSummary = "Set the accuracy for matching fragment peaks. Defaults: 0.01 and 10ppm")
+            public Double setAccuracy(Double mzabs, Double mzppm) throws BioclipseException;
+
+	@PublishedMethod(params = "Integer depth", 
+            methodSummary = "Set the tree depth for fragmentation. Default: 2")
+            public Double setDepth(Integer depth) throws BioclipseException;
+
+	@PublishedMethod(params = "ISpectrum spectrum, IMolecule molecule", 
+            methodSummary = "Runs a canned spectrum against pre-canned molecule")
+            public Double score(ISpectrum spectrum, IMolecule molecule) throws BioclipseException;
+	
+	@PublishedMethod(params = "ISpectrum spectrum, List<IMolecule> molecules", 
+            methodSummary = "Runs a spectrum against several molecules")
+            public List<Double> scores(ISpectrum spectrum, List<IMolecule> molecules) throws BioclipseException;
+
+//	@PublishedMethod(params = "ISpectrum spectrum, IMolecule molecule", 
+//            methodSummary = "Runs a canned spectrum against pre-canned molecule")
+//            public IAtomContainerSet fragments(ISpectrum spectrum, IMolecule molecule) throws BioclipseException;
+
 }
--- plugins/net.bioclipse.metfrag/src/net/bioclipse/metfrag/business/MetfragManager.java
+++ plugins/net.bioclipse.metfrag/src/net/bioclipse/metfrag/business/MetfragManager.java
@@ -10,64 +10,36 @@
  ******************************************************************************/
 package net.bioclipse.metfrag.business;
 
-import java.io.StringReader;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Vector;
 
 import net.bioclipse.core.business.BioclipseException;
 import net.bioclipse.core.domain.IMolecule;
 import net.bioclipse.core.domain.ISpectrum;
-import net.bioclipse.jobs.IReturner;
 import net.bioclipse.managers.business.IBioclipseManager;
-
-import net.bioclipse.core.domain.ISpectrum;
-import net.bioclipse.spectrum.domain.IJumboSpectrum;
-import net.bioclipse.spectrum.domain.JumboSpectrum;
 import net.bioclipse.spectrum.business.SpectrumManager;
-import net.bioclipse.spectrum.business.SpectrumManagerFactory;
+import net.bioclipse.spectrum.domain.JumboSpectrum;
 
 import org.apache.log4j.Logger;
-import org.eclipse.core.runtime.IProgressMonitor;
-import org.openscience.cdk.ChemFile;
-import org.openscience.cdk.ChemObject;
-import org.openscience.cdk.DefaultChemObjectBuilder;
-import org.openscience.cdk.atomtype.CDKAtomTypeMatcher;
-import org.openscience.cdk.exception.CDKException;
-import org.openscience.cdk.exception.InvalidSmilesException;
-import org.openscience.cdk.graph.ConnectivityChecker;
-import org.openscience.cdk.interfaces.IAtom;
+import org.openscience.cdk.AtomContainerSet;
 import org.openscience.cdk.interfaces.IAtomContainer;
-import org.openscience.cdk.interfaces.IAtomType;
-import org.openscience.cdk.io.MDLReader;
-import org.openscience.cdk.smiles.SmilesParser;
-import org.openscience.cdk.tools.CDKHydrogenAdder;
-import org.openscience.cdk.tools.manipulator.AtomContainerManipulator;
-import org.openscience.cdk.tools.manipulator.AtomTypeManipulator;
-import org.openscience.cdk.tools.manipulator.ChemFileManipulator;
-import org.openscience.cdk.tools.manipulator.MolecularFormulaManipulator;
+import org.openscience.cdk.interfaces.IAtomContainerSet;
+import org.xmlcml.cml.element.CMLSpectrum;
+import org.xmlcml.cml.element.CMLPeak;
 
-import de.ipbhalle.metfrag.chemspiderClient.ChemSpider;
-import de.ipbhalle.metfrag.fragmenter.Fragmenter;
-import de.ipbhalle.metfrag.keggWebservice.KeggWebservice;
-import de.ipbhalle.metfrag.main.AssignFragmentPeak;
-import de.ipbhalle.metfrag.main.BioClipseConvenience;
-import de.ipbhalle.metfrag.main.CleanUpPeakList;
-import de.ipbhalle.metfrag.main.MetFlowConvenience;
-import de.ipbhalle.metfrag.main.PeakMolPair;
-import de.ipbhalle.metfrag.main.WrapperSpectrum;
+import de.ipbhalle.metfrag.massbankParser.Spectrum;
 import de.ipbhalle.metfrag.massbankParser.Peak;
-import de.ipbhalle.metfrag.pubchem.PubChemWebService;
-import de.ipbhalle.metfrag.scoring.Scoring;
-import de.ipbhalle.metfrag.tools.MolecularFormulaTools;
-import de.ipbhalle.metfrag.tools.PPMTool;
+
+import net.bioclipse.cdk.domain.ICDKMolecule;
 
 public class MetfragManager implements IBioclipseManager {
 
     private static final Logger logger = Logger.getLogger(MetfragManager.class);
-
+    private Double mzabs = 0.001;
+    private Double mzppm = 10.0;
+    private Integer depth = 2;
+        
     /**
      * Gives a short one word name of the manager used as variable name when
      * scripting.
@@ -76,61 +48,116 @@ public class MetfragManager implements IBioclipseManager {
         return "metfrag";
     }
 
-	public List<Double> calculateMetFragScore()
-			throws BioclipseException {
-		
+    public void setAccuracy(Double mzabs, Double mzppm) 
+    {
+    	this.mzabs = mzabs;
+    	this.mzppm = mzppm;
+    }
+
+    public void setDepth(Integer depth) 
+    {
+    	this.depth = depth;
+    }
+
+    public Double score(ISpectrum spectrum, IMolecule molecule) throws BioclipseException
+    {   	
+        List<IMolecule> molecules = new ArrayList();
+        molecules.add(molecule);
+
+    	List<Double> result = scores(spectrum, molecules); 
+		return result.get(0);
+    }
+    
+    public List<Double> scores(ISpectrum spectrum, List<IMolecule> molecules) throws BioclipseException {
 		List<Double> result = new ArrayList<Double>();
-		// Do calculation
 
-		// Later move ISpectrum spectrum into parameters
-		ISpectrum spectrum = null;
-		
-		SpectrumManager sm = new SpectrumManager();
-		IJumboSpectrum jspectrum = sm.create(spectrum);
-
-		// ... and extract peaks:
-		// jspectrum.getPeakListElements().get(0).getPeakElements().get(x).xValue()
-		
 		String peaks = "119.051 467.616 45\n123.044 370.662 36\n" +
 		"147.044 6078.145 606\n153.019 10000.0 999\n179.036 141.192 13\n189.058 176.358 16\n" +
 		"273.076 10000.000 999\n274.083 318.003 30\n";
+
+		SpectrumManager sm = new SpectrumManager(); 
+		JumboSpectrum jspectrum = (JumboSpectrum) sm.create(spectrum);
+		CMLSpectrum cspectrum = jspectrum.getJumboObject();
+
+		Vector<Peak> mfpeaks = new Vector<Peak>();
+		for (CMLPeak peak : cspectrum.getPeakListElements().getList().get(0).getPeakElements()) {
+			mfpeaks.add(new Peak(peak.getXValue(), peak.getYValue(), -1)); 
+		}
 		
-		String smiles = "C1C(OC2=CC(=CC(=C2C1=O)O)O)C3=CC=C(C=C3)O";
-		IAtomContainer molecule = DefaultChemObjectBuilder.getInstance().newAtomContainer();
 
-		//parse smiles		
-		try {
-			SmilesParser sp = new SmilesParser(DefaultChemObjectBuilder.getInstance());
-			molecule = sp.parseSmiles(smiles);
-			//configure atoms
-			AtomContainerManipulator.percieveAtomTypesAndConfigureAtoms(molecule);
-			//add all hydrogens explicitly
-			CDKHydrogenAdder adder1 = CDKHydrogenAdder.getInstance(molecule.getBuilder());
-			adder1.addImplicitHydrogens(molecule);
-			AtomContainerManipulator.convertImplicitToExplicitHydrogens(molecule);
-		} catch (InvalidSmilesException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		} catch (CDKException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		} 
-
-        // Fake a list of molecules
-        List<IAtomContainer> molecules = new ArrayList<IAtomContainer>();
-        molecules.add(molecule);
-        		
-//      MetFlowConvenience mfc = new MetFlowConvenience(0.01, 50.0, 10.0, peaks, "kegg", true, "", 50, 1, 272.06847);
-//      System.out.println(mfc.metFrag());
+		
+		Spectrum mfspectrum = new Spectrum(-1 /*CE eV */ , 
+											mfpeaks, 
+											0.0 /* mass */ , 1 /*mode*/,  
+											"" /* inchi */ , 0 /*CID*/ , 
+											"" /* KEGG */ , "" /* Name */, "" /*formula*/  );
+		
 
 		try {
-			BioClipseConvenience bcc = new BioClipseConvenience(0.01, 50.0, peaks, molecules, true, "", 1, 272.06847);
+			BioClipseConvenience bcc = new BioClipseConvenience(mzabs, mzppm, mfspectrum, molecules, true, "", 1, 272.06847);
 			logger.info("And the score is...");
 			result = bcc.metFrag();		
 		} catch (Exception e) {
 			System.out.println("Error! TODO...");
 			e.printStackTrace();
 		}
-		return result;		
-     }
+		return result;
+		
+    }
+    
+//
+//    
+//	public List<Double> calculateMetFragScore()
+//			throws BioclipseException {
+//		
+//		List<Double> result = new ArrayList<Double>();
+//		// Do calculation
+//
+//		// For now lets create the ISpectrum spectrum,
+//		// Later move ISpectrum spectrum into method parameters
+//		IJumboSpectrum jtmpspectrum = new JumboSpectrum();
+//        jtmpspectrum.getJumboObject().addPeakList(new CMLPeakList());
+//		ISpectrum spectrum = jtmpspectrum;
+//
+//		// ... and extract peaks:
+//		
+//		String peaks = "119.051 467.616 45\n123.044 370.662 36\n" +
+//		"147.044 6078.145 606\n153.019 10000.0 999\n179.036 141.192 13\n189.058 176.358 16\n" +
+//		"273.076 10000.000 999\n274.083 318.003 30\n";
+//		
+//		String smiles = "C1C(OC2=CC(=CC(=C2C1=O)O)O)C3=CC=C(C=C3)O";
+//		IAtomContainer molecule = DefaultChemObjectBuilder.getInstance().newAtomContainer();
+//
+//		//parse smiles		
+//		try {
+//			SmilesParser sp = new SmilesParser(DefaultChemObjectBuilder.getInstance());
+//			molecule = sp.parseSmiles(smiles);
+//			//configure atoms
+//			AtomContainerManipulator.percieveAtomTypesAndConfigureAtoms(molecule);
+//			//add all hydrogens explicitly
+//			CDKHydrogenAdder adder1 = CDKHydrogenAdder.getInstance(molecule.getBuilder());
+//			adder1.addImplicitHydrogens(molecule);
+//			AtomContainerManipulator.convertImplicitToExplicitHydrogens(molecule);
+//		} catch (InvalidSmilesException e1) {
+//			// TODO Auto-generated catch block
+//			e1.printStackTrace();
+//		} catch (CDKException e1) {
+//			// TODO Auto-generated catch block
+//			e1.printStackTrace();
+//		} 
+//
+//        // Fake a list of molecules
+//        IAtomContainerSet molecules = new AtomContainerSet();
+//        molecules.addAtomContainer(molecule);
+//        		
+//		try {
+//			BioClipseConvenience bcc = new BioClipseConvenience(0.01, 50.0, peaks, molecules, true, "", 1, 272.06847);
+//			logger.info("And the score is...");
+//			result = bcc.metFrag();		
+//		} catch (Exception e) {
+//			System.out.println("Error! TODO...");
+//			e.printStackTrace();
+//		}
+//		return result;		
+//     }
 }
