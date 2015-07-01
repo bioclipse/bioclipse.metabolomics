be57ad1c2b7baf69f5aa05b48bccb6d655db115b
Egon Willighagen <egon.willighagen@gmail.com>
2015-07-01 02:53:25 +0200

    Added a template MSDK manager

---
+++ plugins/net.bioclipse.msdk/src/net/bioclipse/msdk/business/IMsdkManager.java
@@ -0,0 +1,21 @@
+/*******************************************************************************
+ * Copyright (c) 2015  Egon Willighagen <egonw@users.sf.net>
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contact: http://www.bioclipse.net/
+ ******************************************************************************/
+package net.bioclipse.msdk.business;
+
+import net.bioclipse.core.PublishedClass;
+import net.bioclipse.managers.business.IBioclipseManager;
+
+@PublishedClass(
+    value="TODO: Describe the manager here."
+)
+public interface IMsdkManager extends IBioclipseManager {
+
+}
